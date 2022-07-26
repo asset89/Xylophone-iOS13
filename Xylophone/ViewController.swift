8102dba0c6a0a89a1e8ad567edc2c3f92a92a82e
asset89 <asset89@gmail.com>
2022-07-26 08:30:04 +0600

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let letter = sender.currentTitle!
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            sender.alpha = 1.0
+        }
+        playSound(letter)
+        
+    }
     
-
+    func playSound(_ letter: String) {
+        let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
